50fe4cd3ec2848fc63009b1a94c8cba7f5cec0bb
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-11 22:08:01 +0600

    Displayed toolbar and messages container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import Toolbar from './Components/UI/Toolbar/Toolbar.tsx';
+import Messages from './Containers/Messages/Messages.tsx';
 
 const App = () => {
-  return <></>;
+  return <>
+    <Toolbar/>
+
+    <div className="container m-5">
+      <Messages/>
+    </div>
+  </>;
 };
 
 export default App;
