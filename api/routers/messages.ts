116f0e5a4b63e42aa22e814a3aebb85abb5c28d8
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-11 21:39:13 +0600

    1. Created main router file 2. Added 2 types of requests: get & post 3. Added a validation to send an error if the message field is empty 4. Added an ability to upload images

---
+++ api/routers/messages.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import fileDb from "../fileDb";
+import {IMessage, IMessageMutation} from "../types";
+import {imagesUpload} from "../multer";
+
+const messagesRouter = express.Router();
+
+messagesRouter.get('/', async (req, res) => {
+    const messages: IMessage[] = await fileDb.getMessages();
+    res.send(messages.reverse());
+});
+
+messagesRouter.post('/', imagesUpload.single('image'), async (req, res) => {
+    if (!req.body.message) {
+        res.status(400).send({error: 'Please enter a message!'});
+        return;
+    }
+
+    const message: IMessageMutation = {
+        author: req.body.author ? req.body.author : 'Anonymous',
+        message: req.body.message,
+        image: req.file ? 'images' + req.file.filename : null,
+    };
+
+    const newMessage = await fileDb.createMessage(message);
+    res.send(newMessage);
+});
+
+export default messagesRouter;
